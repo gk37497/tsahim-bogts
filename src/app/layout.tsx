16949fe3069ecc1e4dd52727fca095dad7a9e70c
Namsraijamts <namsraijamts.ts@ondo.mn>
2023-11-23 12:37:35 +0800

    feat: changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 import { getStrapiURL } from "./utils/api-helpers";
-import { fetchAPI } from "./utils/fetch-api";
-
 import { FALLBACK_SEO } from "./utils/constants";
+import { fetchAPI } from "./utils/fetch-api";
 
 async function getGlobal(lang: string): Promise<any> {
   const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
@@ -63,6 +63,13 @@ export async function generateMetadata({
   };
 }
 
+export const primaryFont = Nunito({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+});
+
 export default async function RootLayout({
   children,
   params,
@@ -71,7 +78,7 @@ export default async function RootLayout({
   params: { lang: string };
 }) {
   return (
-    <html lang={params.lang}>
+    <html lang={params.lang} className={primaryFont.className}>
       <body>{children}</body>
     </html>
   );
